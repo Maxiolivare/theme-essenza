271acd0bd612b3e735a3806a875505659355cae2
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-11-21 17:00:29 -0300

    PRUEBA AHORA SI

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EssenzaChileVelas</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/scss/estilo.css">
-</head>
-<body>
-    <h1>hola mudno lol</h1>
+<?php get_header();?>
+<main>
+    <h1>HOLAHOLAHOLA</h1>
+</main>
 </body>
 </html>
