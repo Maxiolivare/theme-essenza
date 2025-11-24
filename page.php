ec998884da41eb0b01fb623efcde1554e806a425
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-11-24 19:36:12 -0300

    page

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<main id="primary" class="site-main">
+
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) :
+            the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
+
+</main>
+
+<?php get_footer(); ?>
