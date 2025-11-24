0ccba65f2cb5ad899a1afdb28d6d2e3859e170a7
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-11-24 17:50:27 -0300

    tienda...

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,61 +1,38 @@
 <?php
 /**
  * Custom Shop Page Template
- * Overridden from WooCommerce archive-product.php
+ * Clean version without duplicate loops
+ * Template: archive-product.php
  */
-defined( 'ABSPATH' ) || exit;
-// Abrir contenedor principal
 
-?>
-<?php get_header(); ?>
-  <main>
-      <div class="productos">
-        <h1 class="productos-h1">PRODUCTOS</h1>
-        <ul>
-          <li class="categorias">
-            <a href="#" onclick="mostrarSubmenu(event)">Categorías ▼</a>
-            <!-- SUBMENÚ -->
-            <ul class="submenu" id="lista-categorias">
-              <li><a onclick="mostrarCategoria('florales')">Arreglos florales</a></li>
-              <li><a onclick="mostrarCategoria('gourmet')">Gourmet</a></li>
-              <li><a onclick="mostrarCategoria('flores')">Flores</a></li>
-              <li><a onclick="mostrarCategoria('animales')">Animales</a></li>
-            </ul>
-          </li>
+defined('ABSPATH') || exit();
+
+get_header(); ?>
+
+<main>
+  <div class="productos">
+    <h1 class="productos-h1">PRODUCTOS</h1>
+    <ul>
+      <li class="categorias">
+        <a href="#" onclick="mostrarSubmenu(event)">Categorías ▼</a>
+        <ul class="submenu" id="lista-categorias">
+          <li><a onclick="mostrarCategoria('florales')">Arreglos florales</a></li>
+          <li><a onclick="mostrarCategoria('gourmet')">Gourmet</a></li>
+          <li><a onclick="mostrarCategoria('flores')">Flores</a></li>
+          <li><a onclick="mostrarCategoria('animales')">Animales</a></li>
         </ul>
-      </div>
-      <!-- IMÁGENES -->
-      <div id="contenedor-categorias" class="contenedor-categorias"></div>
-  </main>
-<?php
-if ( woocommerce_product_loop() ) {
-    do_action( 'woocommerce_before_shop_loop' );
+      </li>
+    </ul>
+  </div>
 
-    // ---------- LOOP DE PRODUCTOS ----------
-    woocommerce_product_loop_start();
+  <div id="contenedor-categorias" class="contenedor-categorias"></div>
 
-    if ( wc_get_loop_prop( 'total' ) ) {
-        while ( have_posts() ) {
-            the_post();
-
-            do_action( 'woocommerce_shop_loop' );
-
-            // tarjeta de producto
-            wc_get_template_part( 'content', 'product' );
-        }
-    }
-
-    woocommerce_product_loop_end();
-
-    // ---------- PAGINACIÓN ----------
-    do_action( 'woocommerce_after_shop_loop' );
-
-} else {
-
-    do_action( 'woocommerce_no_products_found' );
-}
-
-do_action( 'woocommerce_after_main_content' );
-?>
+  <?php
+  /**
+   * Aquí WooCommerce maneja el loop completo sin duplicarse.
+   */
+  woocommerce_content();
+  ?>
+</main>
 
 <?php get_footer(); ?>
