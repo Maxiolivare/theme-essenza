723874415b864b0b86ef77d63412fee44e6bf545
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-11-24 16:39:45 -0300

    ahora si la tienda

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 add_theme_support('post-thumbnails');
 
+function mi_tema_soporte_woocommerce() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mi_tema_soporte_woocommerce' );
+
 /* modificar woocommerce */
 add_filter( 'woocommerce_show_page_title', '__return_false' );
