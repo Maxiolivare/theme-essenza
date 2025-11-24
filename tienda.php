9bf1ef97f66fe3baf331fcbe575b0ca0e428c88e
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-11-24 15:40:12 -0300

    ....

--- tienda.php
+++ tienda.php
@@ -1,1 +1,19 @@
-
+  <main>
+      <div class="productos">
+        <h1 class="productos-h1">PRODUCTOS</h1>
+        <ul>
+          <li class="categorias">
+            <a href="#" onclick="mostrarSubmenu(event)">Categorías ▼</a>
+            <!-- SUBMENÚ -->
+            <ul class="submenu" id="lista-categorias">
+              <li><a onclick="mostrarCategoria('florales')">Arreglos florales</a></li>
+              <li><a onclick="mostrarCategoria('gourmet')">Gourmet</a></li>
+              <li><a onclick="mostrarCategoria('flores')">Flores</a></li>
+              <li><a onclick="mostrarCategoria('animales')">Animales</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- IMÁGENES -->
+      <div id="contenedor-categorias" class="contenedor-categorias"></div>
+  </main>
