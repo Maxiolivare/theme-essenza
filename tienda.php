5341ddd5f7c253716fbbb1b4e19a3bba026721b3
Linanat <150732924+Linanat@users.noreply.github.com>
2025-11-24 21:14:10 -0300

    Update tienda.php

--- tienda.php
+++ tienda.php
@@ -1,19 +1,151 @@
-  <main>
-      <div class="productos">
+ 
+<main>
+
+    <div class="productos">
         <h1 class="productos-h1">PRODUCTOS</h1>
-        <ul>
-          <li class="categorias">
-            <a href="#" onclick="mostrarSubmenu(event)">Categorías ▼</a>
-            <!-- SUBMENÚ -->
-            <ul class="submenu" id="lista-categorias">
-              <li><a onclick="mostrarCategoria('florales')">Arreglos florales</a></li>
-              <li><a onclick="mostrarCategoria('gourmet')">Gourmet</a></li>
-              <li><a onclick="mostrarCategoria('flores')">Flores</a></li>
-              <li><a onclick="mostrarCategoria('animales')">Animales</a></li>
-            </ul>
-          </li>
+
+        <ul class="menu-principal">
+            <li class="categorias">
+                <a href="#" class="categorias-link">Categorías ▼</a>
+
+                <!-- SUBMENÚ -->
+                <ul class="submenu">
+                    <li><a href="#" onclick="mostrarCategoria('todos')">Todos</a></li>
+                    <li><a href="#" onclick="mostrarCategoria('florales')">Arreglos florales</a></li>
+                    <li><a href="#" onclick="mostrarCategoria('gourmet')">Gourmet</a></li>
+                    <li><a href="#" onclick="mostrarCategoria('flores')">Flores</a></li>
+                    <li><a href="#" onclick="mostrarCategoria('animales')">Animales</a></li>
+                </ul>
+            </li>
         </ul>
-      </div>
-      <!-- IMÁGENES -->
-      <div id="contenedor-categorias" class="contenedor-categorias"></div>
-  </main>
+    </div>
+
+    <div id="contenedor-categorias" class="contenedor-categorias"></div>
+
+    <!-- sección de productos -->
+
+    <section class="productosVelas">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-01.png" alt="">
+                    <h2>producto#1</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-02.png" alt="">
+                    <h2>producto#2</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-03.png" alt="">
+                    <h2>producto#3</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-04.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-05.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+             <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-06.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            
+             <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-07.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            
+             <div class="col-md-3">
+                <div class="producto gourmet">
+                    <img src="assets/img/fotos-editadas/gourmet-08.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <hr>
+
+                 <div class="col-md-3">
+                <div class="producto florales">
+                    <img src="assets/img/fotos-editadas/arreglo_floral_1.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+             <div class="col-md-3">
+                <div class="producto florales">
+                    <img src="assets/img/fotos-editadas/arreglo_floral_2.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <hr>
+
+               <div class="col-md-3">
+                <div class="producto animales">
+                    <img src="assets/img/fotos-editadas/perro.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+
+            <hr>
+            
+               <div class="col-md-3">
+                <div class="producto flores ">
+                    <img src="assets/img/fotos-editadas/racimo-flor.png" alt="">
+                    <h2>producto#4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h4>$5.000</h4>
+                </div>
+            </div>
+        </div>
+
+        
+    </section>
+
+</main>
