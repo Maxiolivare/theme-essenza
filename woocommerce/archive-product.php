1eb5a590966825eb318836a3a3fa24269a2713cf
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-11-26 12:59:19 -0300

    sdvsd

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -55,10 +55,8 @@ $categorias = get_terms([
                 ]
             ];
         }
-
         $query = new WP_Query($args);
         ?>
-
         <!-- GRID DE PRODUCTOS -->
         <div class="product-grid">
         <?php while ($query->have_posts()) : $query->the_post(); global $product; ?>
@@ -70,7 +68,6 @@ $categorias = get_terms([
             }
             $precio = $product->get_price_html();
             ?>
-
             <article class="product-card">
                 <div class="product-card__image-wrapper">						
                     <a href="<?php the_permalink(); ?>">
@@ -78,7 +75,6 @@ $categorias = get_terms([
                             style="background-image: url('<?php echo esc_url($imagen_principal); ?>');">
                         </div>
                     </a>
-
                     <a 
                         href="<?php echo esc_url( wc_get_cart_url() . '?add-to-cart=' . get_the_ID() ); ?>"
                         class="product-card__add-to-cart"
@@ -87,18 +83,13 @@ $categorias = get_terms([
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icono-carrito.svg" alt="Agregar al carrito">
                     </a>
                 </div>
-
                 <h3 class="product-card__title"><?php the_title(); ?></h3>
                 <p class="product-card__price"><?php echo $precio; ?></p>
             </article>
-
         <?php endwhile; ?>
         </div>
-
         <?php wp_reset_postdata(); ?>
-
     </div>
 </main>
-
 <?php get_footer(); ?>
 
