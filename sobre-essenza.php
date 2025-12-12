b9aedd86aff829f0c8725928891dada18256bf26
Gabriel Neftali Bravo Poveda <150732605+MdeMandiga@users.noreply.github.com>
2025-12-12 18:18:12 -0300

    as

--- sobre-essenza.php
+++ sobre-essenza.php
@@ -45,7 +45,7 @@ if ( have_posts() ) :
         <!-- Imagen derecha -->
         <div class="col-md-2 d-flex justify-content-end">
             <img 
-                src="<?php echo get_template_directory_uri(); ?>/assets/img/flores-sobre-essenza02.png" 
+                src="<?php echo get_template_directory_uri('flores_sobre_essenza_1'); ?>/assets/img/flores-sobre-essenza02.png" 
                 alt="flores-sobre-essenza02" 
                 class="img-fluid img-derecha">
         </div>
