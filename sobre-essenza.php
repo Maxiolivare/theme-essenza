0893c02b2f3f1881bb11d38c6094755fb1c5de8e
Gabriel Neftali Bravo Poveda <150732605+MdeMandiga@users.noreply.github.com>
2025-12-12 19:00:46 -0300

    sep

--- sobre-essenza.php
+++ sobre-essenza.php
@@ -56,7 +56,7 @@ if ( have_posts() ) :
 
     <div class="separador text-center">
         <img 
-            src="<?php echo get_template_directory_uri(); ?>/assets/img/separador.png" 
+            src="<?php echo esc_url( get_field( 'separador' ) ); ?>" 
             alt="linea-separadora" 
             class="img-fluid">
     </div>
@@ -132,7 +132,7 @@ if ( have_posts() ) :
 
     <div class="separador text-center py-5">
         <img 
-            src="<?php echo get_template_directory_uri(); ?>/assets/img/separador.png" 
+            src="<?php echo esc_url( get_field( 'separador' ) ); ?>"  
             alt="linea-separadora" 
             class="img-fluid">
     </div>
