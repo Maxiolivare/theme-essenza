9c2284afddf985c79040c8627ef2e3b6f150662d
Gabriel Neftali Bravo Poveda <150732605+MdeMandiga@users.noreply.github.com>
2025-12-12 19:56:27 -0300

    a

--- sobre-essenza.php
+++ sobre-essenza.php
@@ -107,7 +107,7 @@ if ( have_posts() ) :
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8 mx-auto text-center py-5">
-                <h1 class="text-cinzel">La persona detrás de la marca</h1>
+                <h1 class="text-cinzel fw-bold">La persona detrás de la marca</h1>
             </div>
         </div>
 
